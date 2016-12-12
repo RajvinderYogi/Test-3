57892a409740c472a962260fac783d7f9f96f9df
RajvinderYogi <rajvinderyogi@yahoo.com>
2016-12-12 10:38:52 -0500

    Test-3 Files

---
+++ Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿namespace Test_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lblques7 = new System.Windows.Forms.Label();
+            this.lblQ7 = new System.Windows.Forms.Label();
+            this.Btn_click7 = new System.Windows.Forms.Button();
+            this.CombSeason = new System.Windows.Forms.ComboBox();
+            this.CombMonths = new System.Windows.Forms.ComboBox();
+            this.Btn_ShowMonths = new System.Windows.Forms.Button();
+            this.LblQues6 = new System.Windows.Forms.Label();
+            this.LblQues5 = new System.Windows.Forms.Label();
+            this.LblMoviesResult = new System.Windows.Forms.Label();
+            this.Btn_Movies = new System.Windows.Forms.Button();
+            this.LblQues4 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.BtnLogIn = new System.Windows.Forms.Button();
+            this.LblPassword = new System.Windows.Forms.Label();
+            this.LblQues3 = new System.Windows.Forms.Label();
+            this.LblString = new System.Windows.Forms.Label();
+            this.BtnQues3 = new System.Windows.Forms.Button();
+            this.LblQues2 = new System.Windows.Forms.Label();
+            this.TxtFirstInt = new System.Windows.Forms.TextBox();
+            this.TxtSecondInt = new System.Windows.Forms.TextBox();
+            this.LblAdd = new System.Windows.Forms.Label();
+            this.LblEquals = new System.Windows.Forms.Label();
+            this.Btn_Add = new System.Windows.Forms.Button();
+            this.LblResultAdd = new System.Windows.Forms.Label();
+            this.LblError = new System.Windows.Forms.Label();
+            this.LblQues1 = new System.Windows.Forms.Label();
+            this.LblMyName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Lblques7
+            // 
+            this.Lblques7.AutoSize = true;
+            this.Lblques7.Location = new System.Drawing.Point(61, 52);
+            this.Lblques7.Name = "Lblques7";
+            this.Lblques7.Size = new System.Drawing.Size(54, 17);
+            this.Lblques7.TabIndex = 0;
+            this.Lblques7.Text = "Ques 7";
+            // 
+            // lblQ7
+            // 
+            this.lblQ7.AutoSize = true;
+            this.lblQ7.Location = new System.Drawing.Point(238, 43);
+            this.lblQ7.Name = "lblQ7";
+            this.lblQ7.Size = new System.Drawing.Size(0, 17);
+            this.lblQ7.TabIndex = 1;
+            // 
+            // Btn_click7
+            // 
+            this.Btn_click7.Location = new System.Drawing.Point(181, 89);
+            this.Btn_click7.Name = "Btn_click7";
+            this.Btn_click7.Size = new System.Drawing.Size(75, 28);
+            this.Btn_click7.TabIndex = 2;
+            this.Btn_click7.Text = "Click Here";
+            this.Btn_click7.UseVisualStyleBackColor = true;
+            this.Btn_click7.Click += new System.EventHandler(this.Btn_click7_Click);
+            // 
+            // CombSeason
+            // 
+            this.CombSeason.FormattingEnabled = true;
+            this.CombSeason.Items.AddRange(new object[] {
+            "Spring",
+            "Summer",
+            "Fall",
+            "Winter"});
+            this.CombSeason.Location = new System.Drawing.Point(151, 197);
+            this.CombSeason.Name = "CombSeason";
+            this.CombSeason.Size = new System.Drawing.Size(77, 24);
+            this.CombSeason.TabIndex = 3;
+            // 
+            // CombMonths
+            // 
+            this.CombMonths.FormattingEnabled = true;
+            this.CombMonths.Location = new System.Drawing.Point(282, 197);
+            this.CombMonths.Name = "CombMonths";
+            this.CombMonths.Size = new System.Drawing.Size(91, 24);
+            this.CombMonths.TabIndex = 4;
+            // 
+            // Btn_ShowMonths
+            // 
+            this.Btn_ShowMonths.Location = new System.Drawing.Point(194, 256);
+            this.Btn_ShowMonths.Name = "Btn_ShowMonths";
+            this.Btn_ShowMonths.Size = new System.Drawing.Size(117, 33);
+            this.Btn_ShowMonths.TabIndex = 5;
+            this.Btn_ShowMonths.Text = "Show Months";
+            this.Btn_ShowMonths.UseVisualStyleBackColor = true;
+            this.Btn_ShowMonths.Click += new System.EventHandler(this.Btn_ShowMonths_Click);
+            // 
+            // LblQues6
+            // 
+            this.LblQues6.AutoSize = true;
+            this.LblQues6.Location = new System.Drawing.Point(61, 204);
+            this.LblQues6.Name = "LblQues6";
+            this.LblQues6.Size = new System.Drawing.Size(54, 17);
+            this.LblQues6.TabIndex = 6;
+            this.LblQues6.Text = "Ques 6";
+            // 
+            // LblQues5
+            // 
+            this.LblQues5.AutoSize = true;
+            this.LblQues5.Location = new System.Drawing.Point(61, 332);
+            this.LblQues5.Name = "LblQues5";
+            this.LblQues5.Size = new System.Drawing.Size(54, 17);
+            this.LblQues5.TabIndex = 7;
+            this.LblQues5.Text = "Ques 5";
+            // 
+            // LblMoviesResult
+            // 
+            this.LblMoviesResult.AutoSize = true;
+            this.LblMoviesResult.Location = new System.Drawing.Point(163, 332);
+            this.LblMoviesResult.Name = "LblMoviesResult";
+            this.LblMoviesResult.Size = new System.Drawing.Size(0, 17);
+            this.LblMoviesResult.TabIndex = 8;
+            // 
+            // Btn_Movies
+            // 
+            this.Btn_Movies.Location = new System.Drawing.Point(482, 332);
+            this.Btn_Movies.Name = "Btn_Movies";
+            this.Btn_Movies.Size = new System.Drawing.Size(75, 23);
+            this.Btn_Movies.TabIndex = 9;
+            this.Btn_Movies.Text = "Movies";
+            this.Btn_Movies.UseVisualStyleBackColor = true;
+            this.Btn_Movies.Click += new System.EventHandler(this.Btn_Movies_Click);
+            // 
+            // LblQues4
+            // 
+            this.LblQues4.AutoSize = true;
+            this.LblQues4.Location = new System.Drawing.Point(61, 405);
+            this.LblQues4.Name = "LblQues4";
+            this.LblQues4.Size = new System.Drawing.Size(54, 17);
+            this.LblQues4.TabIndex = 10;
+            this.LblQues4.Text = "Ques 4";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(183, 405);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(55, 22);
+            this.txtPassword.TabIndex = 11;
+            // 
+            // BtnLogIn
+            // 
+            this.BtnLogIn.Location = new System.Drawing.Point(282, 405);
+            this.BtnLogIn.Name = "BtnLogIn";
+            this.BtnLogIn.Size = new System.Drawing.Size(75, 33);
+            this.BtnLogIn.TabIndex = 12;
+            this.BtnLogIn.Text = "Log In";
+            this.BtnLogIn.UseVisualStyleBackColor = true;
+            this.BtnLogIn.Click += new System.EventHandler(this.BtnLogIn_Click);
+            // 
+            // LblPassword
+            // 
+            this.LblPassword.AutoSize = true;
+            this.LblPassword.ForeColor = System.Drawing.Color.ForestGreen;
+            this.LblPassword.Location = new System.Drawing.Point(479, 408);
+            this.LblPassword.Name = "LblPassword";
+            this.LblPassword.Size = new System.Drawing.Size(226, 17);
+            this.LblPassword.TabIndex = 13;
+            this.LblPassword.Text = "You Entered the Correct Password";
+            this.LblPassword.Visible = false;
+            // 
+            // LblQues3
+            // 
+            this.LblQues3.AutoSize = true;
+            this.LblQues3.Location = new System.Drawing.Point(61, 492);
+            this.LblQues3.Name = "LblQues3";
+            this.LblQues3.Size = new System.Drawing.Size(54, 17);
+            this.LblQues3.TabIndex = 14;
+            this.LblQues3.Text = "Ques 3";
+            // 
+            // LblString
+            // 
+            this.LblString.AutoSize = true;
+            this.LblString.Location = new System.Drawing.Point(163, 492);
+            this.LblString.Name = "LblString";
+            this.LblString.Size = new System.Drawing.Size(0, 17);
+            this.LblString.TabIndex = 15;
+            // 
+            // BtnQues3
+            // 
+            this.BtnQues3.Location = new System.Drawing.Point(362, 492);
+            this.BtnQues3.Name = "BtnQues3";
+            this.BtnQues3.Size = new System.Drawing.Size(102, 29);
+            this.BtnQues3.TabIndex = 16;
+            this.BtnQues3.Text = "Click Here";
+            this.BtnQues3.UseVisualStyleBackColor = true;
+            this.BtnQues3.Click += new System.EventHandler(this.BtnQues3_Click);
+            // 
+            // LblQues2
+            // 
+            this.LblQues2.AutoSize = true;
+            this.LblQues2.Location = new System.Drawing.Point(61, 579);
+            this.LblQues2.Name = "LblQues2";
+            this.LblQues2.Size = new System.Drawing.Size(54, 17);
+            this.LblQues2.TabIndex = 17;
+            this.LblQues2.Text = "Ques 2";
+            // 
+            // TxtFirstInt
+            // 
+            this.TxtFirstInt.Location = new System.Drawing.Point(183, 576);
+            this.TxtFirstInt.Name = "TxtFirstInt";
+            this.TxtFirstInt.Size = new System.Drawing.Size(100, 22);
+            this.TxtFirstInt.TabIndex = 18;
+            this.TxtFirstInt.TextChanged += new System.EventHandler(this.TxtFirstInt_TextChanged);
+            // 
+            // TxtSecondInt
+            // 
+            this.TxtSecondInt.Location = new System.Drawing.Point(339, 574);
+            this.TxtSecondInt.Name = "TxtSecondInt";
+            this.TxtSecondInt.Size = new System.Drawing.Size(100, 22);
+            this.TxtSecondInt.TabIndex = 19;
+            this.TxtSecondInt.TextChanged += new System.EventHandler(this.TxtSecondInt_TextChanged);
+            // 
+            // LblAdd
+            // 
+            this.LblAdd.AutoSize = true;
+            this.LblAdd.Location = new System.Drawing.Point(306, 577);
+            this.LblAdd.Name = "LblAdd";
+            this.LblAdd.Size = new System.Drawing.Size(16, 17);
+            this.LblAdd.TabIndex = 20;
+            this.LblAdd.Text = "+";
+            // 
+            // LblEquals
+            // 
+            this.LblEquals.AutoSize = true;
+            this.LblEquals.Location = new System.Drawing.Point(458, 576);
+            this.LblEquals.Name = "LblEquals";
+            this.LblEquals.Size = new System.Drawing.Size(16, 17);
+            this.LblEquals.TabIndex = 21;
+            this.LblEquals.Text = "=";
+            // 
+            // Btn_Add
+            // 
+            this.Btn_Add.Location = new System.Drawing.Point(183, 625);
+            this.Btn_Add.Name = "Btn_Add";
+            this.Btn_Add.Size = new System.Drawing.Size(128, 41);
+            this.Btn_Add.TabIndex = 22;
+            this.Btn_Add.Text = "Multiply Numbers";
+            this.Btn_Add.UseVisualStyleBackColor = true;
+            this.Btn_Add.Click += new System.EventHandler(this.Btn_Add_Click);
+            // 
+            // LblResultAdd
+            // 
+            this.LblResultAdd.AutoSize = true;
+            this.LblResultAdd.Location = new System.Drawing.Point(515, 574);
+            this.LblResultAdd.Name = "LblResultAdd";
+            this.LblResultAdd.Size = new System.Drawing.Size(0, 17);
+            this.LblResultAdd.TabIndex = 23;
+            // 
+            // LblError
+            // 
+            this.LblError.AutoSize = true;
+            this.LblError.ForeColor = System.Drawing.Color.Red;
+            this.LblError.Location = new System.Drawing.Point(327, 631);
+            this.LblError.Name = "LblError";
+            this.LblError.Size = new System.Drawing.Size(141, 17);
+            this.LblError.TabIndex = 24;
+            this.LblError.Text = "Enter a Number Only";
+            this.LblError.Visible = false;
+            // 
+            // LblQues1
+            // 
+            this.LblQues1.AutoSize = true;
+            this.LblQues1.Location = new System.Drawing.Point(61, 698);
+            this.LblQues1.Name = "LblQues1";
+            this.LblQues1.Size = new System.Drawing.Size(54, 17);
+            this.LblQues1.TabIndex = 25;
+            this.LblQues1.Text = "Ques 1";
+            // 
+            // LblMyName
+            // 
+            this.LblMyName.AutoSize = true;
+            this.LblMyName.Location = new System.Drawing.Point(178, 698);
+            this.LblMyName.Name = "LblMyName";
+            this.LblMyName.Size = new System.Drawing.Size(568, 17);
+            this.LblMyName.TabIndex = 26;
+            this.LblMyName.Text = "My Name is Raj,  My student Number is 200353207,  I am going to do Great on this " +
+    "Exam";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 748);
+            this.Controls.Add(this.LblMyName);
+            this.Controls.Add(this.LblQues1);
+            this.Controls.Add(this.LblError);
+            this.Controls.Add(this.LblResultAdd);
+            this.Controls.Add(this.Btn_Add);
+            this.Controls.Add(this.LblEquals);
+            this.Controls.Add(this.LblAdd);
+            this.Controls.Add(this.TxtSecondInt);
+            this.Controls.Add(this.TxtFirstInt);
+            this.Controls.Add(this.LblQues2);
+            this.Controls.Add(this.BtnQues3);
+            this.Controls.Add(this.LblString);
+            this.Controls.Add(this.LblQues3);
+            this.Controls.Add(this.LblPassword);
+            this.Controls.Add(this.BtnLogIn);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.LblQues4);
+            this.Controls.Add(this.Btn_Movies);
+            this.Controls.Add(this.LblMoviesResult);
+            this.Controls.Add(this.LblQues5);
+            this.Controls.Add(this.LblQues6);
+            this.Controls.Add(this.Btn_ShowMonths);
+            this.Controls.Add(this.CombMonths);
+            this.Controls.Add(this.CombSeason);
+            this.Controls.Add(this.Btn_click7);
+            this.Controls.Add(this.lblQ7);
+            this.Controls.Add(this.Lblques7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Lblques7;
+        private System.Windows.Forms.Label lblQ7;
+        private System.Windows.Forms.Button Btn_click7;
+        private System.Windows.Forms.ComboBox CombSeason;
+        private System.Windows.Forms.ComboBox CombMonths;
+        private System.Windows.Forms.Button Btn_ShowMonths;
+        private System.Windows.Forms.Label LblQues6;
+        private System.Windows.Forms.Label LblQues5;
+        private System.Windows.Forms.Label LblMoviesResult;
+        private System.Windows.Forms.Button Btn_Movies;
+        private System.Windows.Forms.Label LblQues4;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button BtnLogIn;
+        private System.Windows.Forms.Label LblPassword;
+        private System.Windows.Forms.Label LblQues3;
+        private System.Windows.Forms.Label LblString;
+        private System.Windows.Forms.Button BtnQues3;
+        private System.Windows.Forms.Label LblQues2;
+        private System.Windows.Forms.TextBox TxtFirstInt;
+        private System.Windows.Forms.TextBox TxtSecondInt;
+        private System.Windows.Forms.Label LblAdd;
+        private System.Windows.Forms.Label LblEquals;
+        private System.Windows.Forms.Button Btn_Add;
+        private System.Windows.Forms.Label LblResultAdd;
+        private System.Windows.Forms.Label LblError;
+        private System.Windows.Forms.Label LblQues1;
+        private System.Windows.Forms.Label LblMyName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_click7_Click(object sender, EventArgs e)
+        {
+            //question 7
+            Double x = 4;
+            Double y = 12;
+            Double result = x / y;
+
+            lblQ7.Text = result.ToString();
+
+        }
+
+        private void Btn_ShowMonths_Click(object sender, EventArgs e)
+        {
+            //question 6
+            String[] Season = new String[3];
+            
+
+            if (CombSeason.SelectedItem.Equals("Spring"))
+            {
+                Season[0] = "March";
+                Season[1] = "April";
+                Season[2] = "May";
+                
+            }
+            else if (CombSeason.SelectedItem.Equals("Summer"))
+            {
+                Season[0] = "June";
+                Season[1] = "July";
+                Season[2] = "August";
+            }
+            else if (CombSeason.SelectedItem.Equals("Fall"))
+            {
+                Season[0] = "September";
+                Season[1] = "October";
+                Season[2] = "November";
+            }
+            else if (CombSeason.SelectedItem.Equals("Winter"))
+            {
+                Season[0] = "December";
+                Season[1] = "January";
+                Season[2] = "February";
+            }
+            for (int i = 0; i <Season.GetLength(0); i++)
+            {
+                CombMonths.Text=Season[i];
+                
+            }
+
+        }
+
+        private void Btn_Movies_Click(object sender, EventArgs e)
+        {
+            //question 5
+            String[,] Movies = new String[3, 2];
+            Movies[0, 0] = " How the Grich Stole Christmas ";
+            Movies[0, 1] = " 1966";
+            Movies[1, 0] = "Frosty the Snowman";
+            Movies[1, 1] = " 1969 ";
+            Movies[2, 0] = "Arthur Christmas";
+            Movies[2, 1] = " 2011 "; 
+            
+            for (var i = 0; i <=3; i++)
+            {
+                LblMoviesResult.Text=(Movies[0, 0] + " ," + Movies[0, 1]) + "\n" + (Movies[1, 0] + " ," + Movies[1, 1]) +"\n" + (Movies[2, 0] + " ," + Movies[2, 1]); 
+               
+            }
+        }
+
+        private void BtnLogIn_Click(object sender, EventArgs e)
+        {
+            //question 4
+            String Password = txtPassword.Text;
+            if (Password=="frosty")
+            {
+                LblPassword.Visible = true;
+            }
+            else
+            {
+                LblPassword.Visible = true;
+                LblPassword.ForeColor= Color.Red;
+                LblPassword.Text = "You Entered the Incorrect Password";
+            }
+        }
+
+        private void BtnQues3_Click(object sender, EventArgs e)
+        {
+            //question 3
+            String[] show = new String[3];
+            show[0] = "Merry";
+            show[1] = "Christmas";
+            show[2] = "C# is Great";
+
+            for (var i = 0; i <3; i++)
+            {
+                LblString.Text = show[0]+" "+show[1] + " "+show[2];
+            }
+
+            }
+
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+            //question 2
+            Int32 First_Number = Convert.ToInt32(TxtFirstInt.Text);
+            Int32 Second_Number = Convert.ToInt32(TxtSecondInt.Text);
+            //calling method here
+            Int32 result = MultiplyNumbers(First_Number, Second_Number);
+            LblResultAdd.Text = "Your Total is: " + result.ToString();
+        }
+        private Int32 MultiplyNumbers(Int32 a, Int32 b)
+        {
+            Int32 total = a * b;
+            return total;
+        }
+
+        private void TxtFirstInt_TextChanged(object sender, EventArgs e)
+        {
+            Int32 validate;
+            if (Int32.TryParse(TxtFirstInt.Text, out validate) == false)
+            {
+                LblError.Visible = true;
+            }
+            else
+            {
+                LblError.Visible = false;
+            }
+        }
+
+        private void TxtSecondInt_TextChanged(object sender, EventArgs e)
+        {
+            Int32 validate;
+            if (Int32.TryParse(TxtSecondInt.Text, out validate) == false)
+            {
+                LblError.Visible = true;
+            }
+            else
+            {
+                LblError.Visible = false;
+            }
+        }
+    }
+}
